c859c54710d324c93c24d27fbf47ce061d8b5c82
netpipe <netpipe@localhost.localdomain>
2021-12-15 15:21:43 -0700

    midibreath

---
+++ MIDI-breath/MIDI-breath.ino
@@ -0,0 +1,195 @@
+//#define USBHID
+#ifdef USBHID
+    #include "MIDIUSB.h"
+#endif
+
+int hits;
+unsigned long startMillis;
+long elapsedmilis = 0;
+
+const int OUT_PIN = 3;
+
+#ifdef memory
+#include <EEPROM.h>
+int addr = 0;
+#endif
+
+byte note = 0;
+
+#define MIDI_ON 144
+#define MIDI_OFF 128
+
+int samplebuffer = 0;
+
+const int sampleWindow = 28; // Sample window width in mS (50 mS = 20Hz)
+const int threshold1 = 26000;    // 350 higher value for larger sample window
+const int threshold2 = 27000;
+//const int threshold4 = 140; 
+const int threshold3 = 28000; //rename to 4 after
+int count = 0;
+
+#include <SWire.h>
+#include <Adafruit_BMP085-swire.h>
+
+/*************************************************** 
+  This is an example for the BMP085 Barometric Pressure & Temp Sensor
+
+  Designed specifically to work with the Adafruit BMP085 Breakout 
+  ----> https://www.adafruit.com/products/391
+
+  These displays use I2C to communicate, 2 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+// Connect VCC of the BMP085 sensor to 3.3V (NOT 5.0V!)
+// Connect GND to Ground
+// Connect SCL to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5
+// Connect SDA to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4
+// EOC is not used, it signifies an end of conversion
+// XCLR is a reset pin, also not used here
+
+Adafruit_BMP085 bmp;
+  
+void setup() {
+    //Wire.pins(sda,slc)
+//  SWire.pins(4,5)
+  //Wire.begin(4,5);
+   #ifdef USBHID
+    Serial.begin(9600);
+              #else
+   // Serial.begin(31250);
+    #endif
+     Serial.begin(9600);
+
+  if (!bmp.begin(3,A4,A5)) {
+	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+	//while (1) {}
+  }
+}
+
+void PrintCount(){
+
+           count++;
+         Serial.println(count);
+         
+}
+  
+void loop() {
+   hits = 9;
+   
+  samplebuffer=0;
+  
+    startMillis = millis(); // Start of sample window
+
+  while (elapsedmilis < sampleWindow)
+  {
+    //if (digitalRead(OUT_PIN) < 1) {
+    //  samplebuffer++;
+   // }
+   
+    samplebuffer += bmp.readPressure();
+    elapsedmilis = millis() - startMillis;
+  }
+  elapsedmilis=0;
+   //first drum loop
+  if (samplebuffer > 0) {
+    Serial.println(samplebuffer);
+
+    if (samplebuffer >= threshold1)
+    {
+      hits = 0;
+      if (samplebuffer >= threshold2)
+      {
+        hits = 1;
+        if (samplebuffer >= threshold3)
+        {
+          hits = 2;
+        }
+      }
+    }
+
+    Serial.println(hits);
+//         switch (hits) {
+//      case 2: {
+//          //noteOn(0x90, note, 127);
+//          #ifdef debug
+//          Serial.println("hit2");
+//          PrintCount();
+//         #endif
+//         #ifdef PLAY
+//          //            delay(3003);
+//          note = 0x4E;
+//          noteOn(0x90, note, 0x45);
+//          delay(10);
+//          //Note on channel 1 (0x90), some note value (note), silent velocity (0x00):
+//          noteOn(0x90, note, 0x00);
+//          #endif
+//          
+//        }break;
+//      case 1: {
+//        #ifdef debug
+//          Serial.println("hit1");
+//          #endif
+//          PrintCount();
+//         #ifdef PLAY
+//            //  for (int note = 0x1E; note < 0x5A; note ++) {
+//            note = 0x5E;
+//            noteOn(0x90, note, 0x45);
+//            delay(10);
+//            //Note on channel 1 (0x90), some note value (note), silent velocity (0x00):
+//            noteOn(0x90, note, 0x00);
+//           #endif 
+//          
+//        }break;
+//      case 0: {
+//        #ifdef debug
+//            Serial.println("hit0");
+//            PrintCount();
+//          #endif
+//         #ifdef PLAY
+//          noteOn(0x90, note, 0x45);
+//          delay(10);
+//          //Note on channel 1 (0x90), some note value (note), silent velocity (0x00):
+//          noteOn(0x90, note, 0x00);
+//      //    delay(10);
+//        #endif
+//       
+//        }   break;
+//    }
+  }
+
+   // Serial.print("Temperature = ");
+   // Serial.print(bmp.readTemperature());
+   // Serial.println(" *C");
+   
+    //Serial.print("Pressure = ");
+   // Serial.print(bmp.readPressure());
+   // Serial.println(" Pa");
+    
+    // Calculate altitude assuming 'standard' barometric
+    // pressure of 1013.25 millibar = 101325 Pascal
+   // Serial.print("Altitude = ");
+   // Serial.print(bmp.readAltitude());
+   // Serial.println(" meters");
+
+  //  Serial.print("Pressure at sealevel (calculated) = ");
+  //  Serial.print(bmp.readSealevelPressure());
+  //  Serial.println(" Pa");
+
+  // you can get a more precise measurement of altitude
+  // if you know the current sea level pressure which will
+  // vary with weather and such. If it is 1015 millibars
+  // that is equal to 101500 Pascals.
+  //  Serial.print("Real altitude = ");
+  //  Serial.print(bmp.readAltitude(104000)); //local is 1040 milibar x 100 = pascals//101500
+ //   Serial.println(" meters");
+    
+   // Serial.println();
+    delay(10);
+}
